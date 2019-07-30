3b3c24127055f73af9c86ff9c4b44591a6139d53
ANN LIN <smilemarg@gmail.com>
2019-07-30 17:59:16 -0400

    order route updated

--- server/api/orders.js
+++ server/api/orders.js
@@ -2,32 +2,79 @@ const router = require('express').Router()
 const {Order, Book} = require('../db/models')
 module.exports = router
 // 'api/orders/'
-// router.get('/', async (req, res, next) => {
-//   try{
-//     const orders = await Order.findAll({
-
-//       includes : [
-//         {model : User}
-//       ]
-//     })
-//   }catch(err){
-//     next(err)
-//   }
-// })
-
-router.get('/:id', async (req, res, next) => {
+// serve up orders for admin
+router.get('/', async (req, res, next) => {
   try {
-    const order = await Order.findById(req.params.id, {
-      includes: [{model: Book}],
+    const orders = await Order.findAll({
       where: {
-        orderbookId: req.params.id
+        completed: true
       }
     })
+    res.send(orders)
   } catch (err) {
     next(err)
   }
 })
 
+// serve up the order at req.params.id eager load order content + quantity
+router.get('/:id', async (req, res, next) => {
+  try {
+    const order = await Order.findById(req.params.id, {
+      where: {
+        orderbookId: req.params.id
+      },
+      includes: [{model: Book}, {model: OrderBook}]
+    })
+    res.send(order)
+  } catch (err) {
+    next(err)
+  }
+})
+
+//buy a book
+router.put('/addToCart/:userId', async (req, res, next) => {
+  // req.body : isbn, quantity { isbn, quantity}
+  try {
+    const book = await Book.findOne({
+      where: {
+        isbn: req.body.isbn
+      }
+    })
+    const order = await Order.findOrCreate({
+      where: {
+        userId: req.params.userId,
+        completed: false
+      },
+      defaults: {
+        completed: false
+      }
+    })
+    await order.addBook(book, {through: {quantity: req.body.quantity}})
+    res.sendStatus(201)
+  } catch (err) {
+    next(err)
+  }
+})
+router.post('/purchase/:userId', async (req, res, next) => {
+  try {
+    const order = await Order.findOne({
+      where: {
+        userId: req.params.userId,
+        completed: false
+      }
+    })
+    if (!order) {
+      res.status(404).send('Cart is empty')
+    } else {
+      await order.update({completed: true})
+      res.sendStatus(201)
+    }
+  } catch (error) {
+    next(error)
+  }
+})
+
+// delete the order at req.params.id
 router.delete('/:id', async (req, res, next) => {
   try {
     const id = await Order.findById(req.params.id)
